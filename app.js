094ddfb6de6c3530dd185f2867cd40c575d33952
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-11 23:52:53 +0300

    Deleting data from the storage.

--- app.js
+++ app.js
@@ -8,14 +8,24 @@ function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross)
+
     cafeList.appendChild(li);
+
+    // Deleting data
+    cross.addEventListener( 'click', (e) => {
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();
+    })
 
 }
 
