3bc0f745f670e05009bd023b8c2fa78054c6f8cf
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-11 23:22:55 +0300

    Getting data from the storage and render it to the DOM.

---
+++ app.js
@@ -0,0 +1,26 @@
+const cafeList = document.querySelector('#cafe-list');
+
+// Create new elements and render cafe-data to the DOM
+
+function renderCafe(doc) {
+
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+    cafeList.appendChild(li);
+
+}
+
+db.collection('cafes').get().then((snapshot) => {
+   snapshot.docs.forEach(doc => {
+       renderCafe(doc)
+       
+   })
+})
